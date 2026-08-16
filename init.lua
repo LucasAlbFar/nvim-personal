71dcdfcf6f940ce0b59e38b5777c894cf28925e2
Lucas Faria <lucasalbfar@gmail.com>
2026-08-16 17:41:47 -0300

    mason lsp imports

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local undodir = vim.fn.expand("~/.vim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
