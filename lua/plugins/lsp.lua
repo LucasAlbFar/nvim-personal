fc11ae92f14e4313066d064931a5370da9fde699
Lucas Albuquerque de Faria <lucas.albuquerquedefaria@tdstelecom.com>
2026-08-17 21:32:49 +0000

    more configs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,24 @@ return {
       servers = {
         pylsp = { enabled = false },
         codebook = { enabled = false },
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+                diagnosticSeverityOverrides = {
+                  reportAny = "none",
+                  reportUnknownArgumentType = "none",
+                  reportUnknownVariableType = "none",
+                  reportUnknownMemberType = "none",
+                  reportUnknownParameterType = "none",
+                  reportMissingParameterType = "none",
+                  reportImplicitOverride = "none",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
