ad1155b028ff88775f137a8be493b9da174b52de
Lucas Albuquerque de Faria <lucas.albuquerquedefaria@tdstelecom.com>
2026-08-19 18:19:57 +0000

    lsp configs one more time

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,22 +13,7 @@ return {
           settings = {
             basedpyright = {
               analysis = {
-                -- "standard" mantém os diagnósticos que apontam bug real
-                -- (nome indefinido, atributo inexistente, argumento errado)
                 typeCheckingMode = "standard",
-                diagnosticSeverityOverrides = {
-                  -- silencia apenas o ruído de código sem type hints
-                  reportAny = "none",
-                  reportUnknownArgumentType = "none",
-                  reportUnknownVariableType = "none",
-                  reportUnknownMemberType = "none",
-                  reportUnknownParameterType = "none",
-                  reportUnknownLambdaType = "none",
-                  reportMissingParameterType = "none",
-                  reportMissingTypeArgument = "none",
-                  reportImplicitOverride = "none",
-                  reportImplicitStringConcatenation = "none",
-                },
               },
             },
           },
