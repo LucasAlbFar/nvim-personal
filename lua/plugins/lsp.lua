94944a8cd1c1bef697df3f013079fc29d0ee1787
Lucas Albuquerque de Faria <lucas.albuquerquedefaria@tdstelecom.com>
2026-08-19 17:16:56 +0000

    lsp configs fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,8 +3,14 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        -- servidores que se anexam a arquivos .py e conflitam com o basedpyright
+        -- (respondem a "definition" sem entender código Python comum,
+        --  além de usarem position encoding diferente)
         pylsp = { enabled = false },
         codebook = { enabled = false },
+        djls = { enabled = false },
+        pytest_language_server = { enabled = false },
+
         basedpyright = {
           settings = {
             basedpyright = {
