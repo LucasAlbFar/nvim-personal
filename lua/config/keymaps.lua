0394147150ab182a3e92b92113e12343df45b702
Lucas Albuquerque de Faria <lucas.albuquerquedefaria@tdstelecom.com>
2026-09-02 14:43:07 +0000

    add keymap to remove backslashes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>=", ":%!black -q -<CR>", { desc = "Format buffer with black" })
+-- remove todas as barras invertidas do buffer (útil para JSON/log escapado)
+vim.api.nvim_create_user_command("StripBackslashes", function()
+  vim.cmd([[%s/\\//g]])
+end, { desc = "Remove all backslashes from buffer" })
+
+vim.keymap.set("n", "<leader>u\\", "<cmd>StripBackslashes<CR>", { desc = "Strip backslashes" })
